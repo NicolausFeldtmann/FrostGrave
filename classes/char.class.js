46f22f22d16844dc9eda1b987f2966e93b0e9c27
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-02-26 16:59:41 +0100

    gravitation an jump function added

--- classes/char.class.js
+++ classes/char.class.js
@@ -1,5 +1,6 @@
 class Char extends MoObject{
 
+    y = 80;
     
     IMAGES_WALKING = [
         'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Running/0_Skeleton_Warrior_Running_000.png',
@@ -15,6 +16,21 @@ class Char extends MoObject{
         'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Running/0_Skeleton_Warrior_Running_010.png',
         'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Running/0_Skeleton_Warrior_Running_011.png',
     ];
+
+    IMAGES_JUMPING = [
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Jump Start/0_Skeleton_Warrior_Jump Start_000.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Jump Start/0_Skeleton_Warrior_Jump Start_001.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Jump Start/0_Skeleton_Warrior_Jump Start_002.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Jump Start/0_Skeleton_Warrior_Jump Start_003.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Jump Start/0_Skeleton_Warrior_Jump Start_004.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Jump Start/0_Skeleton_Warrior_Jump Start_005.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Falling Down/0_Skeleton_Warrior_Falling Down_000.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Falling Down/0_Skeleton_Warrior_Falling Down_001.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Falling Down/0_Skeleton_Warrior_Falling Down_002.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Falling Down/0_Skeleton_Warrior_Falling Down_003.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Falling Down/0_Skeleton_Warrior_Falling Down_004.png',
+        'img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Falling Down/0_Skeleton_Warrior_Falling Down_005.png',
+    ];
     currentImage = 0;
     world;
     speed = 8;
@@ -22,7 +38,8 @@ class Char extends MoObject{
     constructor() {
         super().loadImg('img/witheWalker/Skeleton_Warrior_1/PNG/PNG Sequences/Running/0_Skeleton_Warrior_Running_000.png');
         this.loadImages(this.IMAGES_WALKING);
-        
+        this.loadImages(this.IMAGES_JUMPING);
+        this.appyGravity();
         this.animate();
     }
 
@@ -33,28 +50,30 @@ class Char extends MoObject{
                 this.x += this.speed;
                 this.otherDirection = false;
             }
-        }, 30)
-
-        setInterval(() => {
+            
             if(this.world.keyboard.LEFT && this.x > 100) {
                 this.x -= this.speed;
                 this.otherDirection = true;
             }
-            this.world.camera_x = -this.x + 100;
-        }, 30)
 
-        setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
             this.playAnimation(this.IMAGES_WALKING);
             }
-        }, 40);
-    }
 
-    moveRight() {
+            if(this.world.keyboard.SPACE && !this.isAboveGround()) {
+                this.jump();
+            }
+            this.world.camera_x = -this.x + 100;
+        },1000 / 40);
+
         setInterval(() => {
-            this.x += 0.6;
-        }, 1000 / 60);
+            if(this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
+            }
+        }, 1000 / 30)
     }
-
-    jump() {}
 }
--- classes/moObject.class.js
+++ classes/moObject.class.js
@@ -1,10 +1,13 @@
 class MoObject {
     x = 100;
-    y = 300;
+    y = 320;
     img;
     height = 150;
     width = 100;
     imgCache = {};
+    speed = 0.15;
+    speedY = 0;
+    accel = 2.0;
     otherDirection = false;
 
     loadImg(path) {
@@ -20,6 +23,19 @@ class MoObject {
         });
     }
 
+    appyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.accel;
+            }
+        }, 1000 / 50)
+    }
+
+    isAboveGround() {
+        return this.y < 320;
+    }
+
     playAnimation(images) {
         let i = this.currentImage % this.IMAGES_WALKING.length;
         let path = images[i];
@@ -33,5 +49,9 @@ class MoObject {
 
     moveLeft() {
         console.log('move left');
+    }
+
+    jump() {
+        this.speedY = 25;
     }
 }
