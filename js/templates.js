e073e57610174f758bb0c3d89f47f5c694e3ae8a
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-05-10 12:53:25 +0200

    start screen responsiv update

--- js/templates.js
+++ js/templates.js
@@ -7,8 +7,8 @@ function getWinTemplate() {
     return `
         <div class="endScreen">
             <div class="message">
-                <h2>YOU MADE IT!</h2>
-                <p>
+                <h2 class="lostH2">YOU MADE IT!</h2>
+                <p class="paraLost">
                     Thank you for playing my litte game!<br>
                     It's only a little demo to show what i can code.<br>
                     If you maybe see more of my work, follow the link to my...<br>
